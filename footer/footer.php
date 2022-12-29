aeef6cc65d8f7dd5415c0cb23f464556eb7488a9
Marcelin <marcelintingougoui3@gmail.com>
2022-12-29 16:09:35 +0100

    Dossier qui contient le footer de toute les pages

---
+++ footer/footer.php
@@ -0,0 +1,29 @@
+<div class="fin" style="padding-bottom: 150px;">
+  
+  </div>
+
+ <footer>
+
+  <div class="footer-child2" >
+    <div class="footer-child" style="	position: absolute;	bottom: 0px;	background-color: black;	width: 100%;	color: white; display:flex;align-items:center;">
+  <hr>
+      <div>
+        <p>marcelinTing &copy; 2022 biblioteque.com</p>
+      </div>
+       <div>
+        <p>Bibliothèque</p>
+      </div>
+      <div class="reseau">
+        <div>
+          <a href=""><i class="fab fa-facebook">facebook</i></a>
+        </div>
+        <div>
+          <a href=""><i class="fab fa-instagram-square">instagram</i></a>
+        </div>
+        <div>
+          <a href=""><i class="fab fa-linkedin">linkedin</i></a>
+        </div>
+      </div>
+    </div> 
+  </div>
+</footer> 
