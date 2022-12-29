bfb7691df7f2e12e9f6d1a79b3cb581e73f51da6
Marcelin <marcelintingougoui3@gmail.com>
2022-12-29 12:56:53 +0100

    Ajout de dossier Admin

---
+++ admine/livre.php
@@ -0,0 +1,90 @@
+<?php
+
+include_once("../classe/connection.class.php");
+$pdo= new Connection();
+include_once("../doc.tout/doctout.php");
+if(isset($_GET['message'])){
+  $message=$_GET['message'];
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+  </script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" defert></script>
+  <link rel="stylesheet" href="../disigncss2/style.css">
+  
+</head>
+
+<body>
+<?php 
+include_once("../navbar/navbar.php");
+?>
+<!-- recuperation et affichage des livres -->
+
+  <div class="container">
+  <div class="row">
+    <div class="col-12">
+    <?php if(isset($_GET["susser"]) && isset($_GET["message"])){?>
+      <h2 class="alert ret" id="alert"></h2>
+        <?php } ?>
+      <table class="table">
+        <tr class="th">
+          <th>Auteur</th>
+          <th>Titre</th>
+          <th colspan="2">Action</th>
+
+        </tr>
+        <?php foreach($livre as $livres):?>
+        <tr class="bodytable" >
+          <td><?=$livres['auteur']?></td>
+          <td > <a  href="<?=$livres['fils_dest']?>" style="text-decoration:none;" ><?=$livres['titre']?></a></td>
+          <td><a href="#"><button class="btn btn-del" date-id="<?=$livres['id_livre']?>"><i class="fas fa-trash"></i></a></button><a href="../form/addlivre.php?id=modify&&matricul=<?=$livres['id_livre']?>"><button class="btn "><i class="fas fa-edit"></i></a></button></td>
+        </tr>
+        <?php endforeach;?>
+      </table>
+    </div>
+  </div>
+  </div>
+
+
+  <script>
+  $(document).ready(function () {
+    $('.btn-del').click(function(){
+      if(confirm("Etes vous sur de vouloir supprimer le livre ?")){
+        document.location.href="../crud/addcrud.php?action=delectlivre&&id=abonner&&index="+$(this).attr("date-id");
+        }else{}
+    });
+
+  })
+  </script>
+  <script>
+    
+ document.getElementById("alert").innerHTML = "<?= $message?>"
+
+let setInterval=setTimeout(() => {
+ document.getElementById("alert").style.display = "none";
+}, 2000);
+
+  </script>
+   <div class="fin" style="padding-bottom: 350px;">
+  
+  </div>
+<div class="footer2">
+<?php 
+include_once("../footer/footer.php");
+
+ ?>
+</body>
+
+</html>
